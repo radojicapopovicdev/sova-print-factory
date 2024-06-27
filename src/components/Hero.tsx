d2a6ec7aa9710dd4d95776a0f3114baa039a5072
Radojica Popovic <radojicapopovic@icloud.com>
2024-06-27 19:34:41 +0200

    - Hero section done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-type Props = {}
+// Assets
+import HeroBg from "../assets/perspective-shot-mural.jpg";
+
+type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div id="hero" className="w-full h-[813.33px] flex font-heebo">
+      <Image src={HeroBg} alt={""} className="h-[813.33px]"></Image>
+      <p className="text-[#FFFFFF]/80 absolute text-[24px] mt-[238.67px] ml-[198.67px] ">
+        Urban style
+      </p>
+      <p className="text-[#FFFFFF] absolute text-[46px] mt-[290.67px] ml-[198.67px] w-[672.67px] ">
+        Owl Rebellion is the brand that stands against dullness
+      </p>
+      <div
+        id="button"
+        className="w-[244px] h-[60px] absolute border-white border-[1.3px] ml-[198.67px] mt-[433.67px] rounded-[25.33px] 
+               flex items-center justify-center text-center text-[14px]"
+      >
+        SHOW MORE
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
